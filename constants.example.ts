782cb5bf7a9c94f666e28106c57269a14ad4af33
Ilenia Unida <ileniaunida2493@gmail.com>
2026-01-07 01:58:37 +0100

    Add files via upload

---
+++ constants.example.ts
@@ -0,0 +1,21 @@
+// File: constants.ts
+export const OPENWEATHER_KEY = 'LA_TUA_CHIAVE_QUI'; // Sostituisci con la tua chiave
+
+export const WEATHER_IMAGES: Record<string, string> = {
+    'Clear': 'https://imgur.com/w9Fh7yx.png',
+    'Clouds': 'https://imgur.com/t7DtbfM.png',
+    'Rain': 'https://imgur.com/DJ7SYq7.png',
+    'Drizzle': 'https://imgur.com/DJ7SYq7.png',
+    'Mist': 'https://imgur.com/wTa9gq6.png',
+    'Fog': 'https://imgur.com/wTa9gq6.png',
+    'Haze': 'https://imgur.com/wTa9gq6.png',
+    'Snow': 'https://imgur.com/lT8RV11.png',
+    'Thunderstorm': 'https://imgur.com/DJ7SYq7.png'
+};
+
+export const COLORS = {
+    light: '#F5F2F2',
+    orange: '#FEB05D',
+    blue: '#5A7ACD',
+    dark: '#2B2A2A'
+};
